b1e4f9fd70b77d096ed9f844d66e2ba5dfa90867
Sergio Castro <sacastroc@unal.edu.co>
2017-03-24 15:28:43 -0500

    Realciones fix

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,12 +1,7 @@
 class Purchase < ApplicationRecord
-<<<<<<<<< saved version
 	has_many :Clients
 	has_many :Products
-=========
-	has_many :Clients
-	has_many :Products
-     #Local validations 
+    #Local validations 
     validates :cost, presence: true, numericality: true
     validates :idProduct,payment, presence: true
->>>>>>>>> local version
 end
